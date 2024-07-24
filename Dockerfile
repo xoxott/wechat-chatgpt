19a66d817490013a8624f95317a0ba6f93f54561
yangtao <212920320@qq.com>
2024-07-24 11:39:13 +0800

    refactor : update config

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
-FROM node:19 AS app
+FROM node:16 AS app
 
-# We don't need the standalone Chromium
-RUN apt-get install -y wget \
+# 安装依赖
+RUN apt-get update && apt-get install -y wget gnupg \
     && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list \
-    && apt-get update && apt-get -y install google-chrome-stable chromium  xvfb\
+    && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list \
+    && apt-get update && apt-get install -y google-chrome-stable chromium xvfb \
     && rm -rf /var/lib/apt/lists/* \
     && echo "Chrome: " && google-chrome --version
+
 WORKDIR /app
+
+# 复制并安装npm依赖
 COPY package*.json ./
 RUN npm install
+
+# 复制项目文件
 COPY . .
+
+# 使用xvfb-run运行应用
 CMD xvfb-run --server-args="-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" npm run dev
--- src/openai.ts
+++ src/openai.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: yangtao 212920320@qq.com
+ * @Date: 2024-07-24 11:35:15
+ * @FilePath: \wechat-chatgpt\src\openai.ts
+ * @LastEditTime: 2024-07-24 11:36:42
+ * @LastEditors: yangtao 212920320@qq.com
+ * @Description: 
+ */
 import {
   Configuration,
   CreateImageRequestResponseFormatEnum,
@@ -6,7 +14,7 @@ import {
 } from "openai";
 import fs from "fs";
 import DBUtils from "./data.js";
-import {config} from "./config.js";
+import { config } from "./config.js";
 
 const configuration = new Configuration({
   apiKey: config.openai_api_key,
@@ -17,26 +25,32 @@ const openai = new OpenAIApi(configuration);
 /**
  * Get completion from OpenAI
  * @param username
- * @param message
+ * @param message   
  */
-async function chatgpt(username:string,message: string): Promise<string> {
+async function chatgpt(username: string, message: string): Promise<string> {
   // 先将用户输入的消息添加到数据库中
   DBUtils.addUserMessage(username, message);
   const messages = DBUtils.getChatMessage(username);
   const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-0125",
     messages: messages,
     temperature: config.temperature,
   });
   let assistantMessage = "";
   try {
     if (response.status === 200) {
-      assistantMessage = response.data.choices[0].message?.content.replace(/^\n+|\n+$/g, "") as string;
-    }else{
+      if (response.data.choices && response.data.choices.length > 0) {
+        const choice = response.data.choices[0]
+        if (choice.message && choice.message.content) {
+          assistantMessage = choice.message.content.replace(/^\n+|\n+$/g, "") as string
+        }
+      }
+
+    } else {
       console.log(`Something went wrong,Code: ${response.status}, ${response.statusText}`)
     }
-  }catch (e:any) {
-    if (e.request){
+  } catch (e: any) {
+    if (e.request) {
       console.log("请求出错");
     }
   }
@@ -48,17 +62,17 @@ async function chatgpt(username:string,message: string): Promise<string> {
  * @param username
  * @param prompt
  */
-async function dalle(username:string,prompt: string) {
+async function dalle(username: string, prompt: string) {
   const response = await openai.createImage({
     prompt: prompt,
-    n:1,
+    n: 1,
     size: CreateImageRequestSizeEnum._256x256,
     response_format: CreateImageRequestResponseFormatEnum.Url,
     user: username
   }).then((res) => res.data).catch((err) => console.log(err));
   if (response) {
     return response.data[0].url;
-  }else{
+  } else {
     return "Generate image failed"
   }
 }
@@ -68,15 +82,15 @@ async function dalle(username:string,prompt: string) {
  * @param username
  * @param videoPath
  */
-async function whisper(username:string,videoPath: string): Promise<string> {
-  const file:any= fs.createReadStream(videoPath);
-  const response = await openai.createTranscription(file,"whisper-1")
+async function whisper(username: string, videoPath: string): Promise<string> {
+  const file: any = fs.createReadStream(videoPath);
+  const response = await openai.createTranscription(file, "whisper-1")
     .then((res) => res.data).catch((err) => console.log(err));
   if (response) {
     return response.text;
-  }else{
+  } else {
     return "Speech to text failed"
   }
 }
 
-export {chatgpt,dalle,whisper};
+export { chatgpt, dalle, whisper };
